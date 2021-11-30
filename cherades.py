b8a4f45f446f2cd3a8f0e084439335b9c458d2ab
Nicholas Rengier <32755284+nr36894@users.noreply.github.com>
2021-11-30 10:49:59 -0500

    Game loop

--- cherades.py
+++ cherades.py
@@ -1,5 +1,15 @@
 from datamuse import datamuse
-api = datamuse.Datamuse()
+import pygame
 
-countries = api.words(rel_jja='activity')
-print(countries)
+api = datamuse.Datamuse()
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
