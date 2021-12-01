107d7d5379b6815318f3c65b3560e209f060c963
Nicholas Rengier <32755284+nr36894@users.noreply.github.com>
2021-12-01 17:09:11 -0500

    added comments and a few functions

--- cherades.py
+++ cherades.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import math
+import time
 from datamuse import datamuse
 
 api = datamuse.Datamuse()
@@ -8,9 +9,40 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 
+def addName(name):
+    print("add name")
+    # add the name
+
+
+def getWord():
+    print("get word")
+    # get the word
+
+
+def generateButtons():
+    print("genere buttons")
+    # add remove points
+
+    # show/hide word
+
+    # skip word
+
+
 running = True
 while running:
     screen.fill((0, 0, 0))
+
+    # Display player names
+    
+    # Get and display player scores
+
+    # Display current actor
+
+    # Get and display current word
+
+    # Display buttons
+
+    # Display 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
